9eb9019ae7d4949de0460888bd68a7b3ef7e0865
Woohyun Park <woohyunpark.mail@gmail.com>
2023-04-24 11:56:26 +0900

    Init: 프로젝트 생성
    - localStorage를 이용하여 카운터 임시 구현

---
+++ src/components/Quest.tsx
@@ -0,0 +1,35 @@
+interface IQuest {
+  num: number;
+  txt: string;
+  set: (num: number) => void;
+}
+
+export default function Quest({ num, txt, set }: IQuest) {
+  function handleClick(newNum: number) {
+    if (newNum < 0) set(0);
+    else if (newNum > 3) set(3);
+    else set(newNum);
+  }
+  return (
+    <>
+      <div className="flex items-center justify-between w-full">
+        <h2>{txt}</h2>
+        <div className="flex items-center">
+          <div className="m-2 text-3xl">{num}</div>
+          <div
+            className="m-2 text-lg hover:cursor-pointer"
+            onClick={() => handleClick(num + 1)}
+          >
+            +
+          </div>
+          <div
+            className="m-2 text-lg hover:cursor-pointer"
+            onClick={() => handleClick(num - 1)}
+          >
+            -
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
