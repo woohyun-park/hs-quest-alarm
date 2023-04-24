53dedb89263bbca0c78c58fa0e08e0fa098ddbca
Woohyun Park <woohyunpark.mail@gmail.com>
2023-04-24 14:38:42 +0900

    Feat: Firebase 연동
    - 구글 로그인 기능 구현
    - localStorage에 저장되던 데이터를 firebase로 저장되도록 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,99 @@
+import { auth, db } from "@/apis/firebase";
 import Quest from "@/components/Quest";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
+interface IUser {
+  uid: string;
+  isLoggedIn: boolean;
+}
+
+interface IQuest {
+  daily: number;
+  weekly: number;
+}
+
 export default function Home() {
-  const [quests, setQuests] = useState<{
-    daily: number;
-    weekly: number;
-  }>({
+  const [user, setUser] = useState<IUser>({ uid: "", isLoggedIn: false });
+  const provider = new GoogleAuthProvider();
+  const [quests, setQuests] = useState<IQuest>({
     daily: 0,
     weekly: 0,
   });
 
   function set(type: "daily" | "weekly", num: number) {
-    localStorage.setItem(type, JSON.stringify(num));
+    const docRef = doc(db, "quests", user.uid);
+    updateDoc(docRef, {
+      [type]: num,
+    });
     setQuests({ ...quests, [type]: num });
   }
 
+  function handleLogin() {
+    signInWithPopup(auth, provider)
+      .then(async (res) => {
+        const uid = res.user.uid;
+        setUser({ uid, isLoggedIn: true });
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }
+
+  function handleLogout() {
+    signOut(auth);
+  }
+
   useEffect(() => {
-    function init() {
-      const newDaily = Number(localStorage.getItem("daily"));
-      if (newDaily) set("daily", newDaily);
-      const newWeekly = Number(localStorage.getItem("weekly"));
-      if (newWeekly) set("weekly", newWeekly);
-    }
-    init();
+    auth.onAuthStateChanged(async (authState) => {
+      if (authState) {
+        const uid = authState.uid;
+        const docRef = doc(db, "quests", uid);
+        const docSnap = await getDoc(docRef);
+        const data = docSnap.data();
+        if (!data) await setDoc(docRef, { daily: 0, weekly: 0 });
+        else setQuests({ daily: data.daily, weekly: data.weekly });
+        setUser({ uid: uid, isLoggedIn: true });
+      } else {
+        setUser({ uid: "", isLoggedIn: false });
+      }
+    });
   }, []);
+
   return (
     <>
-      <div className="flex flex-col items-center m-4 max-w-[360px]">
-        <h1 className="mb-8 text-4xl font-bold">하스스톤 퀘스트 알람</h1>
-        <Quest
-          txt="일일 퀘스트"
-          num={quests.daily}
-          set={(num) => set("daily", num)}
-        />
-        <Quest
-          txt="주간 퀘스트"
-          num={quests.weekly}
-          set={(num) => set("weekly", num)}
-        />
-      </div>
+      {!user.isLoggedIn ? (
+        <div className="flex flex-col items-center m-4 max-w-[360px]">
+          <h1 className="mb-2 text-4xl font-bold">하스스톤 퀘스트 알람</h1>
+          <div
+            className="w-full text-right text-gray-500 hover:cursor-pointer"
+            onClick={handleLogin}
+          >
+            로그인
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center m-4 max-w-[360px]">
+          <h1 className="mb-2 text-4xl font-bold">하스스톤 퀘스트 알람</h1>
+          <div
+            className="w-full text-right text-gray-500 hover:cursor-pointer"
+            onClick={() => handleLogout()}
+          >
+            로그아웃
+          </div>
+          <Quest
+            txt="일일 퀘스트"
+            num={quests.daily}
+            set={(num) => set("daily", num)}
+          />
+          <Quest
+            txt="주간 퀘스트"
+            num={quests.weekly}
+            set={(num) => set("weekly", num)}
+          />
+        </div>
+      )}
     </>
   );
 }
